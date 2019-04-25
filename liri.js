bdba00bc7ad9513ad653168e18c05cd99553af47
Jaime Tyler <jaimetyler88@gmail.com>
2019-04-24 23:50:36 -0400

    got started making my variables that are going to bring in the arguments. not having much luck running the first funciton. Keep getting hung up on .keys saying can't find module.

--- liri.js
+++ liri.js
@@ -1,5 +1,44 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+var Spotify = require("node-spotify-api");
+// var spotify = new Spotify({
+//     id: b71fcbca292549488c4142338e7cfd7e,
+//     secret: acb4a851a5d649a7ad98aebb872658a6
+//   });
 
-console.log(keys);
+var command = process.argv[2];
+var subject = process.argv[3];
+
+var request = require('.keys');
+var fs = require('fs');
+
+switch (command) {
+    case 'concert-this':
+        bandsInTown();
+        break;
+   
+}
+
+function bandsInTown() {
+    if (subject == null) {
+        subject = "Butch Walker";
+    }
+    request("https://rest.bandsintown.com/artists/" + subject + "/events?app_id=47972149c9ef95f0470de3a7f2d73af9", function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log("----------------------");
+            console.log("yo what it do");
+        }
+    })
+}
+
+
+ 
+
+ 
+// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+//   if (err) {
+//     return console.log('Error occurred: ' + err);
+//   }
+ 
+// console.log(data); 
+// });
